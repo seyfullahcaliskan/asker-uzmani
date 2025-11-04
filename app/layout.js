bee0d79430e029371d5c88ae14ddf9946a4d605c
seyfullahcaliskan <seyfullah.caliskan@tatmetal.com.tr>
2025-11-04 13:25:00 +0300

    end

--- app/layout.js
+++ app/layout.js
@@ -4,22 +4,22 @@ import "primeicons/primeicons.css";
 import "./globals.css";
 import RootClient from "./RootClient";
 
-export const metadata = {
-  title: "Asker Uzmanı - Özhan Asker Malzemeleri",
-  description:
-    "Özhan Asker Malzemeleri dijitalde! Güvenilir askeri malzeme tedariki ve hızlı sipariş için online mağazamızı keşfedin.",
-  icons: {
-    icon: "/favicon.ico", // Tarayıcı sekmesinde görünen favicon
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico", // iOS için
-  },
-};
+// export const metadata = {
+//   title: "Asker Uzmanı - Özhan Asker Malzemeleri",
+//   description:
+//     "Özhan Asker Malzemeleri dijitalde! Güvenilir askeri malzeme tedariki ve hızlı sipariş için online mağazamızı keşfedin.",
+//   icons: {
+//     icon: "/favicon.ico", // Tarayıcı sekmesinde görünen favicon
+//     shortcut: "/favicon.ico",
+//     apple: "/favicon.ico", // iOS için
+//   },
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body>
-        <RootClient>{children}</RootClient>
+      <body className="bg-black">
+        {/* <RootClient>{children}</RootClient> */}
       </body>
     </html>
   );
